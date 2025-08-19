a8eceae68f2d9c84d63ca3442ca6c8426c15431f
suhanlakshakar89 <suhanlakshakar@gmail.com>
2025-08-19 21:40:28 +0530

    Nature Nest Clone Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from "./Pages/HomePage";  
